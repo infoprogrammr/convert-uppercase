fbb94bf987dd09bc6ee525d13912a1934b79a3e2
infoprogrammr <68546554+infoprogrammr@users.noreply.github.com>
2020-11-18 21:08:49 +0530

    Create Challenge.java

---
+++ Challenge.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
+class touppercase
+{
+  public static void main(String[] args) throws IOException
+  {
+   
+    String str;
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter a string:"); 
+    str = br.readLine();
+
+    /*
+       Your Logic is here
+    */
+
+    System.out.println("Uppercase conversion:");
+    
+  }
+}
+
+
+
+
+
